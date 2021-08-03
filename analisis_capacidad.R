37b46cccbfbc8b0a3f76e5c12b654bb2004c2c9a
Pablo-Aja-Macaya <pablo.aja@udc.es>
2021-08-03 13:55:26 +0200

    Obtener estadísticas de los hospitales en conjunto

--- analisis_capacidad.R
+++ analisis_capacidad.R
@@ -50,11 +50,14 @@ for (h in hospitales){
   row.names(hospital.capacity.stats[[h]]) <- c('mediana','percentil10','percentil90')
   for (u in unidades){
     # Total de camas de este hospital por unidad y ordenadas por fecha
-    tmp <- subset(capacidad, hospital==h & unidad==u)
-    tmp <- tmp[order(tmp$fecha_envio),]
+    datos <- subset(capacidad, hospital==h & unidad==u)
+    datos <- datos[order(datos$fecha_envio),]
+
+    tot <- datos$total_camas
     
-    tot <- tmp$total_camas
-    
+    # Añadir total de ocupadas
+    datos$total_ocupadas <- datos$ocupadas_covid19 + datos$ocupadas_no_covid19
+
     # Mediana y percentiles
     mediana <- median(tot, na.rm=T)
     percentiles <- quantile(tot, probs = seq(0, 1, by= 0.1), na.rm=T)
@@ -66,14 +69,15 @@ for (h in hospitales){
     title(sub=paste('Percentil 90:', percentiles[['90%']]), adj=1, line=4, font=2,cex.sub = 0.75)
     
     # Plot de número de camas a lo largo de la pandemia
-    plot(total_camas ~ fecha_envio, tmp, ylim=c(0,max(tmp$total_camas, na.rm=T)+10), xaxt = "n", type = "l", main=glue('{h} \n({u})'), xlab=NA)
-    lines(ocupadas_no_covid19 ~ fecha_envio, tmp, type="l",lty=1, lwd=0.5, col='blue')
-    lines(ocupadas_covid19 ~ fecha_envio, tmp, type="l",lty=1, lwd=0.5, col='red')
+    plot(total_camas ~ fecha_envio, datos, ylim=c(0,max(datos$total_camas, na.rm=T)+10), xaxt = "n", type = "l", main=glue('{h} \n({u})'), xlab=NA)
+    lines(ocupadas_no_covid19 ~ fecha_envio, datos, type="l",lty=1, lwd=0.5, col='blue')
+    lines(ocupadas_covid19 ~ fecha_envio, datos, type="l",lty=1, lwd=0.5, col='red')
+    lines(total_ocupadas ~ fecha_envio, datos, type="l",lty=1, lwd=0.5, col='green')
     abline(h=mediana, col='darkorchid', lty=1)
     abline(h=percentiles[['10%']], col='deeppink', lty=5)
     abline(h=percentiles[['90%']], col='darkslateblue', lty=5)
     
-    axis.Date(1, at=seq(min(tmp$fecha_envio), max(tmp$fecha_envio), length.out=10), format='%b %Y', las=2, cex.axis=0.8)    
+    axis.Date(1, at=seq(min(datos$fecha_envio), max(datos$fecha_envio), length.out=10), format='%b %Y', las=2, cex.axis=0.8)    
 
     # legend("topright", legend = c('total_camas','COVID19','NO COVID19'),
     #        col = c('black','red','blue'), lty=c(1,1,1), pch = c(NA,NA,NA), bty = "n")
@@ -88,23 +92,29 @@ par(mfrow=c(1,1))
 hospital.capacity.stats
 
 
-tmp <- hospital.capacity.stats[["COMPLEXO HOSPITALARIO UNIVERSITARIO DE SANTIAGO DE COMPOSTELA (CHUS)"]]
-plot(NA, xlim=c(0,100), ylim=c(0,1000))
-abline(h=tmp['Hospitalización convencional']['mediana',], col='blue', lty=1)
-abline(h=tmp['Hospitalización convencional']['percentil10',], col='red', lty=2)
-abline(h=tmp['Hospitalización convencional']['percentil90',], col='red', lty=2)
+# Juntar datos de hospitales para encontrar los stats del conjunto
+merge.hospital.data <- function(hosp.data, parameter){
+  res <- bind_rows(lapply(hosp.data, function(df){df[parameter,]}))
+  res <- apply(res,2,sum)
+  res <- as.data.frame(t(res))
+  rownames(res) <- parameter
+  return(res)
+}
+df_median <- merge.hospital.data(hospital.capacity.stats, 'mediana')
+df_p10 <- merge.hospital.data(hospital.capacity.stats, 'percentil10')
+df_p90 <- merge.hospital.data(hospital.capacity.stats, 'percentil90')
+
+area.capacity.stats <- rbind(df_median, df_p10, df_p90)
 
 
-lapply(hospital.capacity.stats, function(tmp){
-  plot(NA, xlim=c(0,100), ylim=c(0,1000))
-  abline(h=tmp['Hospitalización convencional']['mediana',], col='darkorchid', lty=1)
-  abline(h=tmp['Hospitalización convencional']['percentil10',], col='deeppink', lty=4)
-  abline(h=tmp['Hospitalización convencional']['percentil90',], col='darkslateblue', lty=4)
-  
-})
+plot.capacity.intervals <- function(capacity.stats, unidad){
+  plot(NA, xlim=c(0,100), ylim=c(0,max(area.capacity.stats[,unidad])+10))
+  abline(h=area.capacity.stats['mediana',unidad], col='blue', lty=1)
+  abline(h=area.capacity.stats['percentil10',unidad], col='red', lty=2)
+  abline(h=area.capacity.stats['percentil90',unidad], col='red', lty=2)  
+}
 
-
-tmp <- subset(capacidad, hospital== "COMPLEXO HOSPITALARIO UNIVERSITARIO DE SANTIAGO DE COMPOSTELA (CHUS)"  &
-       unidad=='U. Críticas SIN respirador')
-
+plot.capacity.intervals(area.capacity.stats,'Hospitalización convencional')
+plot.capacity.intervals(area.capacity.stats,'U. Críticas CON respirador')
+plot.capacity.intervals(area.capacity.stats,'U. Críticas SIN respirador')
 
