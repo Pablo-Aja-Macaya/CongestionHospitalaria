39da59846f0b77e82629524f6806826024d7309a
Pablo-Aja-Macaya <pablo.aja@udc.es>
2021-09-09 13:47:11 +0200

    App: plot de capacidad en porcentaje

--- CongestionHospitalariaApp/app.R
+++ CongestionHospitalariaApp/app.R
@@ -520,7 +520,7 @@ server <- function(input, output, session) {
         hospital.capacity.stats <- list()
         
         par(mfrow=c(length(hospitales)*4,2))
-        par(mfrow=c(length(hospitales)*4,2), mar=c(5,4,6,2))
+        par(mfrow=c(length(hospitales)*4,3), mar=c(5,4,6,2))
         for (h in hospitales){
             plot.tittle.line <- 1
             # Inicialización de dataframe de unidades*medidas para el hospital
@@ -552,43 +552,43 @@ server <- function(input, output, session) {
                 title(sub=paste('Percentil 90:', percentiles[['90%']]), adj=1, line=4, font=2,cex.sub = 0.75)
                 title("Histograma de camas", line = plot.tittle.line)
                 
-                # # -- Plot de número de camas a lo largo de la pandemia ----
-                # plot(total_camas ~ fecha_envio, datos, ylim=c(0,max(tot, na.rm=T)+max(tot, na.rm=T)*0.25), xaxt = "n", type = "l", main=NA, xlab=NA, ylab='Camas')
-                # # Columnas rojas (días donde se sobrepasa una de las estadísticas)
-                # # Cuanto más rojizas más gravedad
-                # for (d in min.sobrepasado){
-                #     rect(d-1, 0-20,
-                #          d+1, max(tot, na.rm=T)+80,
-                #          col= rgb(1,0,0,alpha=0.05), lwd=0)      
-                # }
-                # for (d in max.sobrepasado){
-                #     rect(d-1, 0-20,
-                #          d+1, max(tot, na.rm=T)+80,
-                #          col= rgb(1,0,0,alpha=0.3), lwd=0)      
-                # }
-                # for (d in median.sobrepasado){
-                #     rect(d-1, 0-20,
-                #          d+1, max(tot, na.rm=T)+80,
-                #          col= rgb(1,0,0,alpha=0.15), lwd=0)      
-                # }
-                # # Área entre percentil10 y percentil90
-                # rect(fechas[1]-20,percentiles[['10%']],
-                #      fechas[length(fechas)]+20,percentiles[['90%']],
-                #      col= rgb(0,0,1,alpha=0.05), lwd=0)
-                # # Datos de ocupación
-                # lines(ocupadas_no_covid19 ~ fecha_envio, datos, type="l",lty=1, lwd=1, col='blue')
-                # lines(ocupadas_covid19 ~ fecha_envio, datos, type="l",lty=1, lwd=1, col='red')
-                # lines(total_ocupadas ~ fecha_envio, datos, type="l",lty=1, lwd=1, col='green')
-                # abline(h=mediana, col='darkorchid', lty=1)
-                # # abline(h=percentiles[['10%']], col='deeppink', lty=5)
-                # # abline(h=percentiles[['90%']], col='darkslateblue', lty=5)
-                # 
-                # axis.Date(1, at=seq(min(fechas), max(fechas), length.out=10), format='%b %Y', las=2, cex.axis=0.8)    
-                # legend('topright',legend = c('Total camas','Total ocupadas','Ocupadas por COVID','Ocupadas por no COVID','Mediana total camas'), 
-                #        col = c("black","green", "red", "blue", "darkorchid"), lwd = 2, xpd = TRUE, cex = 0.5, bty = 'n')
-                # 
-                # title("Total de camas", line = plot.tittle.line)
-                # 
+                # -- Plot de número de camas a lo largo de la pandemia ----
+                plot(total_camas ~ fecha_envio, datos, ylim=c(0,max(tot, na.rm=T)+max(tot, na.rm=T)*0.25), xaxt = "n", type = "l", main=NA, xlab=NA, ylab='Camas')
+                # Columnas rojas (días donde se sobrepasa una de las estadísticas)
+                # Cuanto más rojizas más gravedad
+                for (d in min.sobrepasado){
+                    rect(d-1, 0-20,
+                         d+1, max(tot, na.rm=T)+80,
+                         col= rgb(1,0,0,alpha=0.05), lwd=0)
+                }
+                for (d in max.sobrepasado){
+                    rect(d-1, 0-20,
+                         d+1, max(tot, na.rm=T)+80,
+                         col= rgb(1,0,0,alpha=0.3), lwd=0)
+                }
+                for (d in median.sobrepasado){
+                    rect(d-1, 0-20,
+                         d+1, max(tot, na.rm=T)+80,
+                         col= rgb(1,0,0,alpha=0.15), lwd=0)
+                }
+                # Área entre percentil10 y percentil90
+                rect(fechas[1]-20,percentiles[['10%']],
+                     fechas[length(fechas)]+20,percentiles[['90%']],
+                     col= rgb(0,0,1,alpha=0.05), lwd=0)
+                # Datos de ocupación
+                lines(ocupadas_no_covid19 ~ fecha_envio, datos, type="l",lty=1, lwd=1, col='blue')
+                lines(ocupadas_covid19 ~ fecha_envio, datos, type="l",lty=1, lwd=1, col='red')
+                lines(total_ocupadas ~ fecha_envio, datos, type="l",lty=1, lwd=1, col='green')
+                abline(h=mediana, col='darkorchid', lty=1)
+                # abline(h=percentiles[['10%']], col='deeppink', lty=5)
+                # abline(h=percentiles[['90%']], col='darkslateblue', lty=5)
+
+                axis.Date(1, at=seq(min(fechas), max(fechas), length.out=10), format='%b %Y', las=2, cex.axis=0.8)
+                legend('topright',legend = c('Total camas','Total ocupadas','Ocupadas por COVID','Ocupadas por no COVID','Mediana total camas'),
+                       col = c("black","green", "red", "blue", "darkorchid"), lwd = 2, xpd = TRUE, cex = 0.5, bty = 'n')
+
+                title("Total de camas", line = plot.tittle.line)
+
                 
                 # -- Plot de porcentaje de ocupación a lo largo de la pandemia ----
                 ocupados.covid.pct <- (datos$ocupadas_covid19/datos$total_camas)*100
